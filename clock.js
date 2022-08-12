2f97fc8b35d9ec466054b55ff5a24cf5ebbb3a07
heojieun <gjekdmlwl@naver.com>
2022-08-12 10:17:27 +0900

    오타 수정

--- clock.js
+++ clock.js
@@ -10,7 +10,7 @@ const clockCheck = () => {
   let second = now.getSeconds();
   if(hour < 10) hour = '0' + hour;
   if(minute < 10) minute = '0' + minute;
-  if(minute < 10) minute = '0' + minute;
+  if(second < 10) second = '0' + second;
 
   $clock.innerText = `${hour}:${minute}:${second}`;
 }
